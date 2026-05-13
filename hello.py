cf42a7ef945af02021535bc707a9f25df2adab21
Mounika <srimounikakv@gmail.com>
2026-05-13 15:20:59 +0530

    changed bye msg

--- hello.py
+++ hello.py
@@ -2,7 +2,7 @@ def greet(name):
     return f"Hello, {name}!"
 
 def farewell(name):
-    return f"Goodbye, {name}!"
+    return f"tata, {name}!"
 
 if __name__ == "__main__":
     print(greet("World"))
